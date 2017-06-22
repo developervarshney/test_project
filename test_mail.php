e27b35dfe2fc4078a866e6536587e271adbb4b7b
ravi <developer.varshney@gmail.com>
2017-06-22 14:03:01 +0530

    update emailid

--- test_mail.php
+++ test_mail.php
@@ -1,8 +1,7 @@
 <?php
-    $to = 'cool.varshney@gmail.com';
+    $to = 'developer.varshney@gmail.com';
     $subject = 'Test Email';
     $headers = "From: ravi.prakash@amvrin.com\r\n";
-    
     $body = 'This is a test Email';
     
     if(@mail($to, $subject, $body, $headers)){
